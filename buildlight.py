78776232c1b5b92fbde0a2c0670948eec9e3a65c
Matt Drago <matt@drago.com.au>
2013-05-30 21:15:02 +1000

    Changed indentation to be consistent

--- buildlight.py
+++ buildlight.py
@@ -11,149 +11,148 @@ except Exception:
 	pass # only required for mac os
 
 class UsbLed:
-    def __init__(self):
-        self.color_method_map = { 'red':self.red, 'green':self.green, 'blue':self.blue, 'off':self.off }
+	def __init__(self):
+		self.color_method_map = { 'red':self.red, 'green':self.green, 'blue':self.blue, 'off':self.off }
+		atexit.register(self.off)
 
-	atexit.register(self.off)
+	def red(self):
+		self.send(0x02)
 
-    def red(self):
-        self.send(0x02)
+	def green(self):
+		self.send(0x01)
 
-    def green(self):
-        self.send(0x01)
+	def blue(self):
+		self.send(0x04)
 
-    def blue(self):
-        self.send(0x04)
+	def off(self):
+		self.send(0x00)
 
-    def off(self):
-        self.send(0x00)
-
-    def set_color(self, color):
-        if color in self.color_method_map.keys():
-	    self.color_method_map[color]()
+	def set_color(self, color):
+		if color in self.color_method_map.keys():
+			self.color_method_map[color]()
 
 class UsbLedGen1(UsbLed):
-    
-    def __init__(self):
-        UsbLed.__init__(self)
-        self.dev = usb.core.find(idVendor=0x0fc5, idProduct=0x1223)
-        if self.dev is None:
-            raise ValueError('Device not found')
+	
+	def __init__(self):
+		UsbLed.__init__(self)
+		self.dev = usb.core.find(idVendor=0x0fc5, idProduct=0x1223)
+		if self.dev is None:
+			raise ValueError('Device not found')
 
-        if self.dev.is_kernel_driver_active(0) is True:
-            self.dev.detach_kernel_driver(0)
+		if self.dev.is_kernel_driver_active(0) is True:
+			self.dev.detach_kernel_driver(0)
 
-        self.dev.set_configuration()
+		self.dev.set_configuration()
 
-    def send(self, color):
-        try:
-            self.dev.ctrl_transfer(bmRequestType=0x000000c8,
-                                   bRequest= 0x00000012,
-                                   wValue=(0x02 * 0x100) + 0x0a,
-                                   wIndex=0xff & (~color),
-                                   data_or_wLength=0x00000008)
-        
-        # a pipe error is thrown even if the operation is successful
-        except usb.core.USBError:
-            pass
+	def send(self, color):
+		try:
+			self.dev.ctrl_transfer(bmRequestType=0x000000c8,
+					bRequest= 0x00000012,
+					wValue=(0x02 * 0x100) + 0x0a,
+					wIndex=0xff & (~color),
+					data_or_wLength=0x00000008)
+		
+		# a pipe error is thrown even if the operation is successful
+		except usb.core.USBError:
+			pass
 
 class UsbLedGen2(UsbLed):
-    
-    def __init__(self):
-        UsbLed.__init__(self)
-        self.dev = usb.core.find(idVendor=0x0fc5, idProduct=0xb080)
-        if self.dev is None:
-            raise ValueError('Device not found')
+	
+	def __init__(self):
+		UsbLed.__init__(self)
+		self.dev = usb.core.find(idVendor=0x0fc5, idProduct=0xb080)
+		if self.dev is None:
+			raise ValueError('Device not found')
 
-        if self.dev.is_kernel_driver_active(0) is True:
-            self.dev.detach_kernel_driver(0)
+		if self.dev.is_kernel_driver_active(0) is True:
+			self.dev.detach_kernel_driver(0)
 
-        self.dev.set_configuration()
+		self.dev.set_configuration()
 
-    def send(self, color):
-        try:
-            self.dev.ctrl_transfer(bmRequestType=0x21,
-                                   bRequest= 0x09,
-                                   wValue=0x0635,
-                                   wIndex=0x000,
-                                   data_or_wLength='\x65\x0c' + chr(color) + '\xff\x00\x00\x00\x00')
-        
-        # a pipe error is thrown even if the operation is successful
-        except usb.core.USBError:
-            pass
+	def send(self, color):
+		try:
+			self.dev.ctrl_transfer(bmRequestType=0x21,
+					bRequest= 0x09,
+					wValue=0x0635,
+					wIndex=0x000,
+					data_or_wLength='\x65\x0c' + chr(color) + '\xff\x00\x00\x00\x00')
+		
+		# a pipe error is thrown even if the operation is successful
+		except usb.core.USBError:
+			pass
 
 class UsbLedFinder:
-    def __init__(self, ledAddress):
-        self.supported_platforms = ['darwin', 'linux']
-	self.current_platform = os.uname()[0].lower()
-	self.idVendor = 0x0fc5
-	self.supportedIdProductsMap = { 0x1223: UsbLedGen1, 0xb080: UsbLedGen2 }
-	self.ledAddress = ledAddress
+	def __init__(self, ledAddress):
+		self.supported_platforms = ['darwin', 'linux']
+		self.current_platform = os.uname()[0].lower()
+		self.idVendor = 0x0fc5
+		self.supportedIdProductsMap = { 0x1223: UsbLedGen1, 0xb080: UsbLedGen2 }
+		self.ledAddress = ledAddress
 
-    def is_current_platform_supported(self):
-	return platform in self.supported_platforms
+	def is_current_platform_supported(self):
+		return platform in self.supported_platforms
 
-    def get_usbled(self):
-        if(self.is_current_platform_supported):
-	    deviceProductId = self.attached_device_product_id()
-	    if deviceProductId in self.supportedIdProductsMap:
-	        return self.supportedIdProductsMap[deviceProductId]()
-        else:
-	    print 'This platform (%s) is not supported' % self.platform
-	    sys.exit(1)
+	def get_usbled(self):
+		if(self.is_current_platform_supported):
+			deviceProductId = self.attached_device_product_id()
+			if deviceProductId in self.supportedIdProductsMap:
+				return self.supportedIdProductsMap[deviceProductId]()
+		else:
+			print 'This platform (%s) is not supported' % self.platform
+			sys.exit(1)
 
-    def attached_device_product_id(self):
-        if self.ledAddress is None:
-            devices = usb.core.find(find_all=True, idVendor=0x0fc5)
-	else:
-            devices = usb.core.find(find_all=True, idVendor=0x0fc5, address=self.ledAddress)
+	def attached_device_product_id(self):
+		if self.ledAddress is None:
+			devices = usb.core.find(find_all=True, idVendor=0x0fc5)
+		else:
+			devices = usb.core.find(find_all=True, idVendor=0x0fc5, address=self.ledAddress)
 
-	if len(devices) == 0:
-	    print 'No devices found.  Exiting'
-	    sys.exit(1)
-	else:
-	    return devices[0].idProduct
+		if len(devices) == 0:
+			print 'No devices found.  Exiting'
+			sys.exit(1)
+		else:
+			return devices[0].idProduct
 
 class HudsonBuildLight:
-    def __init__(self, host, port, jobs, ledAddress=None):
-        self.host = host
-        self.port = port
-        self.jobs = jobs
-        self.usbled = UsbLedFinder(ledAddress).get_usbled()
-        
-        # not mapped colors will default to blue
-        # other colors returned by hudson: blue_anime red_anime grey grey_anime aborted
-        self.color_map = { 'blue':'green', 'blue_anime':'green', 'red':'red', 'red_anime':'red', 'green':'green' }
-        self.default_color = 'red'
+	def __init__(self, host, port, jobs, ledAddress=None):
+		self.host = host
+		self.port = port
+		self.jobs = jobs
+		self.usbled = UsbLedFinder(ledAddress).get_usbled()
+		
+		# not mapped colors will default to blue
+		# other colors returned by hudson: blue_anime red_anime grey grey_anime aborted
+		self.color_map = { 'blue':'green', 'blue_anime':'green', 'red':'red', 'red_anime':'red', 'green':'green' }
+		self.default_color = 'red'
 
-    def get_job_color(self,job):
-        try:
-            conn = httplib.HTTPConnection(self.host, self.port)
-            conn.request('GET','/job/%s/api/python' % job)
-            jobdata = eval(conn.getresponse().read())
-        except Exception:
-            return self.default_color
-        
-        job_color = jobdata['color']
-        if self.color_map.has_key(job_color):
-            return self.color_map[job_color]
-        else:
-            return self.default_color
+	def get_job_color(self,job):
+		try:
+			conn = httplib.HTTPConnection(self.host, self.port)
+			conn.request('GET','/job/%s/api/python' % job)
+			jobdata = eval(conn.getresponse().read())
+		except Exception:
+			return self.default_color
+		
+		job_color = jobdata['color']
+		if self.color_map.has_key(job_color):
+			return self.color_map[job_color]
+		else:
+			return self.default_color
 
-    def get_color(self):
-        colors = map((lambda job: self.get_job_color(job)),self.jobs)
-        if(all(colors[0] == i for i in colors)):
-            return colors[0]
-        else:
-	    return self.default_color
+	def get_color(self):
+		colors = map((lambda job: self.get_job_color(job)),self.jobs)
+		if(all(colors[0] == i for i in colors)):
+			return colors[0]
+		else:
+			return self.default_color
 
-    def loop(self):
-        self.usbled.set_color(self.default_color)
-        last_color = self.get_color()
-        self.usbled.set_color(last_color)
-        while True:
-            color = self.get_color()
-            if color != last_color:
-                self.usbled.set_color(color)
-                last_color = color
-            time.sleep(1)
+	def loop(self):
+		self.usbled.set_color(self.default_color)
+		last_color = self.get_color()
+		self.usbled.set_color(last_color)
+		while True:
+			color = self.get_color()
+			if color != last_color:
+				self.usbled.set_color(color)
+				last_color = color
+			time.sleep(1)
--- run.py
+++ run.py
@@ -2,6 +2,6 @@
 from buildlight import HudsonBuildLight
 
 if __name__ == '__main__':
-    build_light = HudsonBuildLight(host='127.0.0.1', port=8080, jobs=['job-1-here','job-2-here'])
-    build_light.loop()
+	build_light = HudsonBuildLight(host='127.0.0.1', port=8080, jobs=['job-1-here','job-2-here'])
+	build_light.loop()
 
